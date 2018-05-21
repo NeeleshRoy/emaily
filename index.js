0c6a878a849e5fe2cc2adfb9169ed3e8b4c54f16
Neelesh Roy <neeleshroy.2013@gmail.com>
2018-05-21 23:57:58 +0530

    added initial g+ outh

--- index.js
+++ index.js
@@ -5,6 +5,24 @@ const app = express();
 const keys = require('./config/keys');
 const PORT = process.env.PORT || 5000;
 
-passport.use(new GoogleStrategy());
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: keys.google.web.client_id,
+			clientSecret: keys.google.web.client_secret,
+			callbackURL: '/auth/google/callback'
+		},
+		(accessToken) => {
+			console.log(accessToken);
+		}
+	)
+);
+
+app.get(
+	'/auth/google',
+	passport.authenticate('google', {
+		scope: [ 'profile', 'email' ]
+	})
+);
 
 app.listen(PORT);

5725660ed606683259d977278e099e3dbf92cc5a
Neelesh Roy <neeleshroy.2013@gmail.com>
2018-05-22 13:18:40 +0530

    Mongoose Model

--- index.js
+++ index.js
@@ -1,33 +1,13 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
+const mongoose = require('mongoose');
 const keys = require('./config/keys');
+
+require('./services/passport');
+require('./routes/AuthRoutes')(app);
+
+mongoose.connect(keys.mongo.uri);
+
 const PORT = process.env.PORT || 5000;
-
-passport.use(
-	new GoogleStrategy(
-		{
-			clientID: keys.google.web.client_id,
-			clientSecret: keys.google.web.client_secret,
-			callbackURL: '/auth/google/callback'
-		},
-		(accessToken, refreshToken, profile, done) => {
-			console.log(accessToken);
-			console.log(refreshToken);
-			console.log(profile);
-			console.log(done);
-		}
-	)
-);
-
-app.get(
-	'/auth/google',
-	passport.authenticate('google', {
-		scope: [ 'profile', 'email' ]
-	})
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
 
 app.listen(PORT);
